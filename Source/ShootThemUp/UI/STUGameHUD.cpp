c34374205dae7836643425071bb9753c0b5e1f7e
Andrii Shchochka <55036016+Aladin273@users.noreply.github.com>
2024-04-18 18:04:32 +0300

    GameHUD, Drawing, Crosshair

---
+++ Source/ShootThemUp/UI/STUGameHUD.cpp
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "STUGameHUD.h"
+
+#include "Engine/Canvas.h"
+
+void ASTUGameHUD::DrawHUD()
+{
+    FVector2D Center = FVector2D(Canvas->SizeX * 0.5f, Canvas->SizeY * 0.5f);
+
+    float LineSize = 10.f;
+    float LineThickness = 4.0f;
+    FLinearColor LineColor = FLinearColor::White;
+
+    DrawLine(Center.X - LineSize, Center.Y, Center.X + LineSize, Center.Y, LineColor, LineThickness);
+    DrawLine(Center.X, Center.Y - LineSize, Center.X, Center.Y + LineSize, LineColor, LineThickness);
+}
